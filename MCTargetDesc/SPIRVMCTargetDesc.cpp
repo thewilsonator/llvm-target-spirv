2027ae6a7447b4127dfbb3852b0e5bb2b76cc489
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-07-17 18:34:49 +0800

    Clean up a bit

--- MCTargetDesc/SPIRVMCTargetDesc.cpp
+++ MCTargetDesc/SPIRVMCTargetDesc.cpp
@@ -10,26 +10,7 @@
 /// This file provides SPIRV-specific target descriptions.
 ///
 //===----------------------------------------------------------------------===//
-/*
-#include "SPIRVMCTargetDesc.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/MC/MCAsmInfo.h"
-#include "llvm/MC/MCInstrInfo.h"
-#include "llvm/MC/MCRegisterInfo.h"
-#include "llvm/MC/MCStreamer.h"
-#include "llvm/MC/MCSubtargetInfo.h"
-#include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/TargetRegistry.h"
 
-#define GET_INSTRINFO_MC_DESC
-#include "RISCVGenInstrInfo.inc"
-
-#define GET_REGINFO_MC_DESC
-#include "RISCVGenRegisterInfo.inc"
-
-using namespace llvm;
-
-*/
 extern "C" void LLVMInitializeSPIRVTargetMC() {
   //We do not support Jitting
 }
