24713fff1ec65bb8cacb2f672134fe76204fb1d3
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-10-03 10:54:54 +0800

    `tool_output_file` -> ToolOutputFile

--- libSPIRV/SPIRVUtil.cpp
+++ libSPIRV/SPIRVUtil.cpp
@@ -100,7 +100,7 @@ removeCast(Value *V) {
 void
 saveLLVMModule(Module *M, const std::string &OutputFile) {
   std::error_code EC;
-  tool_output_file Out(OutputFile.c_str(), EC, sys::fs::F_None);
+  ToolOutputFile Out(OutputFile.c_str(), EC, sys::fs::F_None);
   if (EC) {
     SPIRVDBG(errs() << "Fails to open output file: " << EC.message();)
     return;
